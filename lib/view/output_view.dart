9792bcdf75bff96600591e925d993f9dbfb1af94
Kiran Rana <kiranrana8973@gmail.com>
2023-10-06 10:11:08 +0545

    Added routing argumebts

--- lib/view/output_view.dart
+++ lib/view/output_view.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-class OutputView extends StatelessWidget {
+class OutputView extends StatefulWidget {
   const OutputView({super.key});
 
   @override
+  State<OutputView> createState() => _OutputViewState();
+}
+
+class _OutputViewState extends State<OutputView> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    int result = ModalRoute.of(context)!.settings.arguments as int;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Output'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Text(
+                'Result : $result',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
